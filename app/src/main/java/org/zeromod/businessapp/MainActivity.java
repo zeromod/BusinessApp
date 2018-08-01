ccceeb20d9a0f637b66de05ee54129cd4d445361
zeromod <zeromoddev@gmail.com>
2018-08-01 08:07:01 +0530

    Initial commit

---
+++ app/src/main/java/org/zeromod/businessapp/MainActivity.java
@@ -0,0 +1,16 @@
+package org.zeromod.businessapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView logo = findViewById(R.id.logo);
+        logo.setClipToOutline(true);
+    }
+}
